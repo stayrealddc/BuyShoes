ed7742639ff7c1b306b8abf8cd0d679c2b2ddf49
daidc <49504225+stayrealddc@users.noreply.github.com>
2019-04-16 09:54:46 +0800

    Update time_run.py

--- logtype/time_run.py
+++ logtype/time_run.py
@@ -5,21 +5,14 @@ from multiprocessing.dummy import Pool as ThreadPool
 import selenium
 from selenium import webdriver
 import yaml
-from PIL import Image, ImageEnhance
+from PIL import Image
 from pyzbar import pyzbar
 from selenium.webdriver.chrome.options import Options
 
 def doSth(person):
     # 设定买鞋二维码，修改baidu.png文件名，二维码放在桌面上
-    # image = "C:\\Users\\admin\\Desktop\\baidu.png"
     image = "C:\\Users\\admin\\Desktop\\IMG_2362.PNG"
     img = Image.open(image)
-
-    #二维码图片清晰可忽略这些步骤
-    # img = ImageEnhance.Brightness(img).enhance(2.0)  # 增加亮度
-    # img = ImageEnhance.Sharpness(img).enhance(17.0)  # 锐利化
-    # img = ImageEnhance.Contrast(img).enhance(4.0)  # 增加对比度
-    # img = img.convert('L')  # 灰度化
 
     # 识别二维码
     barcodes = pyzbar.decode(img)
@@ -42,11 +35,6 @@ def doSth(person):
     sel = selenium.webdriver.Chrome(options=options)
     sel.get(barcodeData)
 
-    #百度网页测试步骤
-    # sel.find_element_by_xpath('//*[@id="kw"]').send_keys(person)
-    # sel.find_element_by_xpath('//*[@id="su"]').click()
-    # time.sleep(1)
-
     # 登记操作
     sel.find_element_by_id("q1").send_keys(name)
     sel.find_element_by_id("q2").send_keys(number)
@@ -55,7 +43,7 @@ def doSth(person):
     sel.find_element_by_xpath('//*[@id="ctlNext"]').click()
     print('===========已经登记过啦===========')
 
-def main(h=9, m=2):
+def main(h=9, m=0):
     #设定买鞋时间，h表示设定的小时，m为设定的分钟
     while True:
         while True:
@@ -66,7 +54,7 @@ def main(h=9, m=2):
             time.sleep(1)
 
         #登记人员名单
-        items = ['ddc', 'ljq']
+        items = ['aaa', 'bbb']
         pool = ThreadPool()
         pool.map(doSth, items)
         pool.close()
